5ba616c62aaafe8a0cbe2cf1e8427ec126b0f3bd
Ernie Oporto <ernieoporto@yahoo.com>
2015-07-11 11:09:00 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -70,16 +70,16 @@
 
     for (CCNode *bush in _bushes) {
         CGPoint offset = bush.position;
-        [self removeChild:bush];
+        //[self removeChild:bush];
         [_parallaxBackground addChild:bush z:0 parallaxRatio:_bushParallaxRatio positionOffset:offset];
     }
-/*
+
     for (CCNode *cloud in _clouds) {
         CGPoint offset = cloud.position;
-        [self removeChild:cloud];
+        //[self removeChild:cloud];
         [_parallaxBackground addChild:cloud z:0 parallaxRatio:_cloudParallaxRatio positionOffset:offset];
     }
-*/
+
     for (CCNode *ground in _grounds) {
         // set collision txpe
         ground.physicsBody.collisionType = @"level";
